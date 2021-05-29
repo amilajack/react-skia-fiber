93f3df94bfd61d9004ddf036bd65ebf2a545ab1f
Amila Welihinda <amilajack@gmail.com>
2021-05-28 19:18:50 -0700

    chore: inital cursor refactor

--- demos/text-editor/App.tsx
+++ demos/text-editor/App.tsx
@@ -1,5 +1,5 @@
-import { FontMgr } from "canvaskit-wasm";
-import React, { useRef, useEffect, useState } from "react";
+import { CanvasKit, FontMgr, LineMetrics, Paragraph } from "canvaskit-wasm";
+import React, { useRef, useEffect, useState, useLayoutEffect } from "react";
 import {
   FontManagerProvider,
   useFontManager,
@@ -8,20 +8,158 @@ import {
   useCanvasKit,
   invalidate,
   toSkColor,
+  useFrame,
 } from "../../src";
 import alice from "./alice";
 import "./style.css";
 
-const PADDING = 800;
-
 const clamp = (val: number, min: number, max: number) =>
   Math.max(min, Math.min(val, max));
+
+class Position {
+  line = 0;
+  column = 0;
+  constructor(line: number, column: number) {
+    this.line = line;
+    this.column = column;
+  }
+  /**
+   * Skia's Paragraph module handles cursor state using indicies of a char
+   * insted of row/col
+   */
+  static fromIndex(index: number, paragraph: Paragraph) {
+    const lineMetrics = paragraph.getLineMetrics();
+    const line = lineMetrics.findIndex(
+      (metric) =>
+        index >= metric.startIndex && index <= metric.endExcludingWhitespaces
+    );
+    return new Position(line, index - lineMetrics[line].startIndex);
+  }
+}
+
+class Cursor {
+  position: Position;
+  index: number;
+  paragraph: Paragraph;
+  width = 5;
+  private lineMetrics: LineMetrics[];
+
+  constructor(paragraph: Paragraph, position?: Position) {
+    this.paragraph = paragraph;
+    this.updateLineMetrics();
+    this.position = position ?? Position.fromIndex(0, paragraph);
+    this.index = this.getIndex();
+  }
+  moveY(deltaY: number) {
+    const nextLine = clamp(
+      this.position.line + deltaY,
+      0,
+      this.lineMetrics.length - 1
+    );
+    const nextLineMetric = this.lineMetrics[nextLine];
+    const currLineMetric = this.lineMetrics[this.position.line];
+
+    const index = this.getIndex();
+    const nextIndex = Math.min(
+      nextLineMetric.startIndex + index - currLineMetric.startIndex,
+      nextLineMetric.endIndex
+    );
+    this.moveToIndex(nextIndex);
+  }
+  moveX(deltaX: number) {
+    const index = this.getIndex();
+    const nextIndex = clamp(
+      index + deltaX,
+      0,
+      this.lineMetrics[this.lineMetrics.length - 1].endIndex
+    );
+    this.moveToIndex(nextIndex);
+  }
+  moveUp() {
+    this.moveY(-1);
+  }
+  moveDown() {
+    this.moveY(1);
+  }
+  moveLeft() {
+    this.moveX(-1);
+  }
+  moveRight() {
+    this.moveX(1);
+  }
+  moveTop() {
+    this.moveToIndex(0);
+  }
+  moveBottom() {
+    this.moveToIndex(
+      this.lineMetrics[this.lineMetrics.length - 1].endIndex - 1
+    );
+  }
+  moveToIndex(index: number) {
+    this.position = Position.fromIndex(index, this.paragraph);
+    this.index = index;
+  }
+  moveToStartOfWord() {
+    const { start } = this.getWordBoundary();
+    if (start === this.getIndex())
+      this.moveToIndex(
+        this.paragraph.getWordBoundary(Math.max(0, start - 1)).start
+      );
+    else this.moveToIndex(start);
+  }
+  moveToStartOfLine() {
+    const lineMetrics = this.lineMetrics[this.position.line];
+    this.moveToIndex(lineMetrics.startIndex);
+  }
+  moveToEndOfLine() {
+    const lineMetrics = this.lineMetrics[this.position.line];
+    this.moveToIndex(lineMetrics.endExcludingWhitespaces);
+  }
+  moveToEndOfWord() {
+    const { end } = this.getWordBoundary();
+    this.moveToIndex(end);
+  }
+  private getWordBoundary() {
+    const index = this.getIndex();
+    return this.paragraph.getWordBoundary(index);
+  }
+  private clone() {
+    return new Cursor(this.paragraph);
+  }
+  setParagraph(paragraph: Paragraph) {
+    this.paragraph = paragraph;
+  }
+  getIndex(): number {
+    return (
+      this.lineMetrics[this.position.line].startIndex + this.position.column
+    );
+  }
+  updateLineMetrics() {
+    this.lineMetrics = this.paragraph.getLineMetrics();
+  }
+  getCursorSelection(
+    ck: CanvasKit,
+    index: number = this.getIndex()
+  ): [number, number, number, number] | undefined {
+    const [rect] = this.paragraph!.getRectsForRange(
+      index,
+      index + 1,
+      ck.RectWidthStyle.Max,
+      ck.RectHeightStyle.Max
+    );
+    if (rect) {
+      return rect;
+    }
+  }
+}
+
+const PADDING = 800;
 
 function TextEditor({ fontMgr }: { fontMgr: FontMgr }) {
   const ck = useCanvasKit();
   const rParagraph = useRef<SkParagraph>();
-  const rIndex = useRef(0);
-  const rCursor = useRef({
+  const rCursor = useRef<Cursor>();
+  const rCursorRect = useRef({
     x: PADDING,
     y: 0,
     w: 0,
@@ -30,23 +168,37 @@ function TextEditor({ fontMgr }: { fontMgr: FontMgr }) {
 
   const textarea = useRef(document.querySelector("textarea")!);
 
-  const insertText = (text: string) => {
-    const prevText = rParagraph.current!.text;
-    const nextIndex = rIndex.current;
-    rParagraph.current!.text = [
-      prevText.slice(0, nextIndex),
-      text,
-      prevText.slice(nextIndex),
-    ].join("");
+  const updateParagraph = (text: string) => {
+    rParagraph.current!.text = text;
     rParagraph.current!.build();
     rParagraph.current!.layout();
-    moveCursor(text.length);
+    rCursor.current.paragraph = rParagraph.current.object;
+    rCursor.current!.updateLineMetrics();
     invalidate();
   };
 
+  const insertText = (text: string) => {
+    const prevText = rParagraph.current!.text;
+    const nextIndex = rCursor.current.getIndex();
+    updateParagraph(
+      [prevText.slice(0, nextIndex), text, prevText.slice(nextIndex)].join("")
+    );
+    rCursor.current?.moveX(text.length);
+    invalidate();
+  };
+
+  useFrame(() => {
+    const cursorRect = rCursor.current.getCursorSelection(ck);
+    const [x, y, w, h] = cursorRect;
+    rCursorRect.current.width = 5;
+    rCursorRect.current.height = h - y;
+    rCursorRect.current.x = x + PADDING;
+    rCursorRect.current.y = y;
+    rCursorRect.current.dirtyLayout = true;
+  });
+
   useEffect(() => {
     rParagraph.current!.fontManager = fontMgr;
-    rParagraph.current!.text = alice.slice(0, 8_000);
     rParagraph.current!.textStyle = new ck.ParagraphStyle({
       textStyle: {
         color: toSkColor(ck, "#d1d1d1"),
@@ -57,9 +209,12 @@ function TextEditor({ fontMgr }: { fontMgr: FontMgr }) {
       maxLines: 100,
       ellipsis: "...",
     });
-    rParagraph.current!.build();
     rParagraph.current!.width = innerWidth * 2 - PADDING * 2;
+    rParagraph.current!.text = alice.slice(0, 8_000);
+    rParagraph.current!.build();
     rParagraph.current!.layout();
+    rCursor.current = new Cursor(rParagraph.current!.object!);
+    focusTextarea();
     invalidate();
   }, []);
 
@@ -70,19 +225,7 @@ function TextEditor({ fontMgr }: { fontMgr: FontMgr }) {
       e.pageY * 2
     );
     if (posA) {
-      const [rect] = skParagraph!.getRectsForRange(
-        posA.pos,
-        posA.pos + 1,
-        ck.RectWidthStyle.Max,
-        ck.RectHeightStyle.Max
-      );
-      console.log(posA, rect, rParagraph.current?.text.charAt(posA.pos));
-      const [x, y, _w, h] = rect;
-      rCursor.current.width = 5;
-      rCursor.current.height = h - y;
-      rCursor.current.x = x + PADDING;
-      rCursor.current.y = y;
-      rIndex.current = posA.pos;
+      rCursor.current?.moveToIndex(posA.pos);
       rCursor.current.dirtyLayout = true;
       invalidate();
     }
@@ -90,7 +233,9 @@ function TextEditor({ fontMgr }: { fontMgr: FontMgr }) {
 
   function handleInput(e) {
     const text = e.target.value;
-    (e.target.value = ""), insertText(text), focusTextarea();
+    e.target.value = "";
+    insertText(text);
+    focusTextarea();
   }
   const handleTextareaKeydown = (e) => {
     const text = e.target.value;
@@ -129,68 +274,44 @@ function TextEditor({ fontMgr }: { fontMgr: FontMgr }) {
     };
   }, []);
 
-  const moveCursor = (cursorDelta: number, deltaY = 0) => {
-    const skParagraph = rParagraph.current!.object!;
-    const metrics = skParagraph.getLineMetrics();
-    let nextIndex = rIndex.current;
-
-    if (deltaY === 0) {
-      nextIndex = clamp(
-        rIndex.current + cursorDelta,
-        0,
-        metrics[metrics.length - 1].endIndex
-      );
-    } else {
-      const metricIndex = metrics.findIndex(
-        (metric) =>
-          rIndex.current >= metric.startIndex &&
-          rIndex.current <= metric.endExcludingWhitespaces
-      );
-      console.log(metrics, metricIndex);
-      if (metricIndex > -1) {
-        const nextLineMetric =
-          metrics[clamp(metricIndex + deltaY, 0, metrics.length - 1)];
-        const currLineMetric = metrics[metricIndex];
-
-        nextIndex = Math.min(
-          nextLineMetric.startIndex +
-            rIndex.current -
-            currLineMetric.startIndex,
-          nextLineMetric.endIndex
-        );
-      }
-    }
-    const [rect] = skParagraph!.getRectsForRange(
-      nextIndex,
-      nextIndex + 1,
-      ck.RectWidthStyle.Max,
-      ck.RectHeightStyle.Max
-    );
-    if (rect) {
-      const [x, y, _w, h] = rect;
-      rCursor.current.width = 5;
-      rCursor.current.height = h - y;
-      rCursor.current.x = x + PADDING;
-      rCursor.current.y = y;
-      rCursor.current.dirtyLayout = true;
-      rIndex.current = nextIndex;
-      invalidate();
-    }
-  };
-
   const handleKeydown = (e) => {
+    invalidate();
+    const cursor = rCursor.current!;
     switch (e.key) {
       case "ArrowLeft":
-        moveCursor(-1);
+        e.metaKey
+          ? cursor.moveToStartOfLine()
+          : e.altKey
+          ? cursor.moveToStartOfWord()
+          : cursor.moveLeft();
         break;
       case "ArrowRight":
-        moveCursor(1);
+        e.metaKey
+          ? cursor.moveToEndOfLine()
+          : e.altKey
+          ? cursor.moveToEndOfWord()
+          : cursor.moveRight();
         break;
       case "ArrowUp":
-        moveCursor(0, -1);
+        e.metaKey ? cursor.moveTop() : cursor.moveUp();
         break;
       case "ArrowDown":
-        moveCursor(0, 1);
+        e.metaKey ? cursor.moveBottom() : cursor.moveDown();
+        break;
+      case "c":
+        if (e.metaKey) {
+          cursor.moveDown();
+        }
+        break;
+      case "a":
+        if (e.metaKey) {
+          cursor.moveDown();
+        }
+        break;
+      case "x":
+        if (e.metaKey) {
+          cursor.moveDown();
+        }
         break;
       // Prevent default tab behavior
       case "Tab":
@@ -199,26 +320,17 @@ function TextEditor({ fontMgr }: { fontMgr: FontMgr }) {
         break;
       // @TODO: Handle shift + tab
       case "Backspace": {
-        const nextIndex = rIndex.current - 1;
+        const index = cursor!.getIndex() - 1;
         const prevText = rParagraph.current!.text;
-        const newText =
-          prevText.slice(0, nextIndex) + prevText.slice(nextIndex + 1);
-        rParagraph.current!.text = newText;
-        rParagraph.current!.build();
-        rParagraph.current!.layout();
-        moveCursor(-1);
-        invalidate();
+        const newText = prevText.slice(0, index) + prevText.slice(index + 1);
+        updateParagraph(newText);
+        cursor.moveLeft();
         break;
       }
       case "Delete":
-        const nextIndex = rIndex.current;
+        const index = cursor!.getIndex();
         const prevText = rParagraph.current!.text;
-        const newText =
-          prevText.slice(0, nextIndex) + prevText.slice(nextIndex + 1);
-        rParagraph.current!.text = newText;
-        rParagraph.current!.build();
-        rParagraph.current!.layout();
-        invalidate();
+        updateParagraph(prevText.slice(0, index) + prevText.slice(index + 1));
         break;
       default: {
         focusTextarea();
@@ -234,7 +346,7 @@ function TextEditor({ fontMgr }: { fontMgr: FontMgr }) {
         y={0}
         width={window.innerWidth * 2}
       />
-      <skRrect ref={rCursor} style={{ color: "#3477f4" }} />
+      <skRrect ref={rCursorRect} style={{ color: "#3477f4" }} />
       {/* <skText
         ref={rText}
         text={`x: ${cursor.x}, y: ${cursor.y}, index: ${cursor.current.index}`}
